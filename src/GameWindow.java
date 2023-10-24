88b10c5f54766d7a520e6040997193b4073d414d
lennart3211 <82011287+lennart3211@users.noreply.github.com>
2023-10-24 16:17:09 -0500

    Update GameWindow.java

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,21 +1,38 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class GameWindow extends JFrame 
 {
     private final int BOARD_SIZE = 64;
     private JButton[] squares = new JButton[BOARD_SIZE];
+    private Color boardColor1 = Color.decode("#282828");
 
     public GameWindow() 
     {
-        this.setSize(300, 300);
+        this.setSize(800, 800);
         Container container = this.getContentPane();
         container.setLayout(new GridLayout(8,8));
 
         for (int i = 0; i < BOARD_SIZE; i++) 
         {
-            squares[i] = new JButton("test");
+            squares[i] = new JButton("Button " + (i + 1));
+            if (((i / 8) % 2 == 0 && i % 2 != 0) || ((i / 8) % 2 != 0 && i % 2 == 0)) 
+            {
+                squares[i].setBackground(boardColor1);
+                squares[i].setForeground(boardColor1);
+            }
+            final int buttonIndex = i;
+            squares[i].addActionListener(new ActionListener() 
+            {
+                public void actionPerformed(ActionEvent e) {buttonClicked(buttonIndex);}
+            });
+            
             container.add(squares[i]);
         }
+    }
+
+    private void buttonClicked(int buttonIndex) {
+        
     }
 }   
